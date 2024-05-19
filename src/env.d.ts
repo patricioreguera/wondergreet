4d9aa679b8398dfeea67dda3035cb3e3607dde84
patricioreguera <reguerapatriciohernan@gmail.com>
2024-05-19 19:28:41 +0200

    TEst10

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+ASTRO_STUDIO_APP_TOKEN: "09de807a629298eba26e4d0e33d540f4a7d47866:qhxqp7vbqj9eo9o32upiut716imu"
